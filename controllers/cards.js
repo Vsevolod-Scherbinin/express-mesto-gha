6976a3eb911e4e8e4907b1c9111ae1199f0ddb1b
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-10-27 15:51:23 +0300

    Щербинин В.О. Проектная работа №13 - v.1.6

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,7 +27,7 @@ module.exports.createCard = (req, res) => {
 }
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.id).orFail(new Error ('NotFound'))
+  Card.findByIdAndRemove(req.params.cardId).orFail(new Error ('NotFound'))
   .then(card => res.send({ data: card }))
   .catch((err) => {
     if(err.name === 'CastError') {
