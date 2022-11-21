68d07180f43eef97baf6793b4e797f53acba9082
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-21 13:06:15 +0300

    Щербинин В.О. Проектная работа №14 - v.3.3.4 CORS App.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const { signInValidation, signUpValidation } = require('./middlewares/requestsVa
 const { clearCookie } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const centralErrorHandler = require('./middlewares/centralErrorHandler');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const cors = require('./middlewares/cors');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -26,30 +25,30 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(requestLogger);
-
 const allowedCors = [
   'https://scherbinin.mesto.nomoredomains.club',
   'http://scherbinin.mesto.nomoredomains.club',
-  'https://api.scherbinin.mesto.nomoredomains.club',
-  'http://api.scherbinin.mesto.nomoredomains.club',
   'localhost:3000',
 ];
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
+
   return next();
 });
 
@@ -62,7 +61,6 @@ app.use(routesCards);
 
 app.use((req, res, next) => next(new NotFoundError('Некорректный адрес запроса')));
 
-app.use(errorLogger);
 app.use(errors());
 app.use(centralErrorHandler);
 
