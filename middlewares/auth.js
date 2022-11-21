35b9b44438c1abae0e6ce0830d07886c61a7d367
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-21 16:55:30 +0300

    Щербинин В.О. Проектная работа №14 - v.3.5 401MainPage

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwtoken = require('jsonwebtoken');
-const { JWT_SECRET } = require('../constants/constants');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // eslint-disable-next-line consistent-return
@@ -13,11 +14,11 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwtoken.verify(jwt, JWT_SECRET);
+    payload = jwtoken.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
-  next();
+  return next();
 };
