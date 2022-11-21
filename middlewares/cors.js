ce90b0a8141a435e63027f5ed309ee945d280c9a
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-21 11:44:10 +0300

    Щербинин В.О. Проектная работа №14 - v.3.2.1 CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ const allowedCors = [
   'localhost:3000',
 ];
 
-module.exports = (req, res, next) => {
+module.exports.cors = (req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
