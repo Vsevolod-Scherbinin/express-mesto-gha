972b6fb9438ce79306b53ead0c81484d13493ed8
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-21 21:45:09 +0300

    Щербинин В.О. Проектная работа №14 - v.3.7

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
 const { cardValidation, cardIdValidation } = require('../middlewares/requestsValidation');
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.use(auth);
-router.get('/cards', getCards);
-router.post('/cards', cardValidation, createCard);
-router.delete('/cards/:cardId', cardIdValidation, deleteCard);
-router.put('/cards/:cardId/likes', cardIdValidation, likeCard);
-router.delete('/cards/:cardId/likes', cardIdValidation, dislikeCard);
+router.get('/', getCards);
+router.post('/', cardValidation, createCard);
+router.delete('/:cardId', cardIdValidation, deleteCard);
+router.put('/:cardId/likes', cardIdValidation, likeCard);
+router.delete('/:cardId/likes', cardIdValidation, dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,26 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
-const { userIdValidation, userDataValidation, userAvatarValidation } = require('../middlewares/requestsValidation');
+const {
+  userIdValidation,
+  userDataValidation,
+  userAvatarValidation,
+} = require('../middlewares/requestsValidation');
 
 const {
-  getUsers, getUserById, editUser, editAvatar,
+  getUsers,
+  getUserById,
+  editUser,
+  editAvatar,
+  clearCookie,
 } = require('../controllers/users');
 
 // eslint-disable-next-line max-len
 // Важна последовательность. :userId должен быть после get-запроса по users/me. Альтернатива - отдельный контроллер на users/me
-router.use(auth);
-router.get('/users', getUsers);
-router.get('/users/me', getUserById);
-router.patch('/users/me', userDataValidation, editUser);
-router.patch('/users/me/avatar', userAvatarValidation, editAvatar);
-router.get('/users/:userId', userIdValidation, getUserById);
+
+router.get('/', getUsers);
+router.get('/me', getUserById);
+router.delete('/signout', clearCookie);
+router.patch('/me', userDataValidation, editUser);
+router.patch('/me/avatar', userAvatarValidation, editAvatar);
+router.get('/:userId', userIdValidation, getUserById);
 
 module.exports = router;
