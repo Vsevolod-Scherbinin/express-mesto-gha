307f9a148f4244fbdba497bc0ae527215dfb3ee0
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-21 15:07:53 +0300

    Щербинин В.О. Проектная работа №14 - v.3.4.2 CORS Middleware NoName

--- app.js
+++ app.js
@@ -28,35 +28,6 @@ app.use(requestLogger);
 
 app.use(cors);
 
-// const allowedCors = [
-//   'https://scherbinin.mesto.nomoredomains.club',
-//   'http://scherbinin.mesto.nomoredomains.club',
-//   'https://api.scherbinin.mesto.nomoredomains.club',
-//   'http://api.scherbinin.mesto.nomoredomains.club',
-//   'localhost:3000',
-// ];
-
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-
-//   const { method } = req;
-
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-
-//   return next();
-// });
-
 app.post('/signup', signUpValidation, createUser);
 app.post('/signin', signInValidation, login);
 app.get('/signout', clearCookie);
