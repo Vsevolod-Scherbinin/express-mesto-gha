db432334cdbfd5f5e1f8fdc986211453d7c173ea
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-11-22 10:44:46 +0300

    Щербинин В.О. Проектная работа №14 - v.3.9.10 logout

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use(requestLogger);
 
 app.use(cors);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', signUpValidation, createUser);
 app.post('/signin', signInValidation, login);
 app.get('/signout', clearCookie);
--- controllers/users.js
+++ controllers/users.js
@@ -128,7 +128,7 @@ module.exports.login = (req, res, next) => {
     });
 };
 
-module.exports.clearCookie = (req, res) => {
+module.exports.logout = (req, res) => {
   res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
     .send({ message: 'Вы вышли из аккаунта' });
 };
